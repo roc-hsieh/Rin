00626e9a0779df5bbbacc59688a1224c7f8723f3
ahsieh <roc.ahsieh@gmail.com>
2025-10-16 16:59:41 +0800

    fix: 解决问题

--- scripts/migrator.ts
+++ scripts/migrator.ts
@@ -41,7 +41,7 @@ Bun.write('wrangler.toml', stripIndent(`
 #:schema node_modules/wrangler/config-schema.json
 name = "${WORKER_NAME}"
 main = "server/src/_worker.ts"
-compatibility_date = "2024-05-29"
+compatibility_date = "2024-09-23"
 compatibility_flags = ["nodejs_compat"]
 # node_compat = true
 
